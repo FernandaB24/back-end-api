68ffe508ca8b5dd1160dd6b9314803884f4db24c
FernandaB24 <fbr2@aluno.ifnmg.edu.br>
2025-09-23 17:29:01 +0000

    qwe

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
 import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
-app.get("/", (req, res) => {        // Cria endpoint na rota da raiz do projeto
+dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
+
+app.get("/", async (req, res) => {        // Cria endpoint na rota da raiz do projeto
   console.log("Rota GET / solicitada");
+
+  const db = new Pool({  
+  connectionString: process.env.URL_BD,
+});
+
+let dbStatus = "ok";
+try {
+  await db.query("SELECT 1");
+} catch (e) {
+  dbStatus = e.message;
+}
+
   res.json({
 		message: "API para .....",      // Substitua pelo conteúdo da sua API
     author: "FERNANDA BARBOSA RODRIGUES",    // Substitua pelo seu nome
